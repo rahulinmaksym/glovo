e40af10998c3793bf6f37cfb169e9cabef5ef33a
rahulinmaksym <rahulinmaksym@gmail.com>
2024-01-30 23:44:28 +0200

    Added logging. Homework 24 is done.

--- glovoHillel/src/main/java/org/raghoul818/glovohillel/service/order/OrderServiceImpl.java
+++ glovoHillel/src/main/java/org/raghoul818/glovohillel/service/order/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package org.raghoul818.glovohillel.service.order;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.raghoul818.glovohillel.dto.order.OrderDto;
 import org.raghoul818.glovohillel.dto.product.ProductDto;
 import org.raghoul818.glovohillel.mapper.order.OrderMapper;
@@ -12,6 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
@@ -19,60 +21,76 @@ public class OrderServiceImpl implements OrderService {
     private final OrderMapper orderMapper;
     private final ProductService productService;
 
-    private Order optionalToEntity(Optional<Order> optionalOfOrder) {
-        Order order = new Order();
-        if(optionalOfOrder.isPresent()) {
-            order = optionalOfOrder.get();
-        }
-        return order;
-    }
-
     @Override
     public OrderDto getById(Integer id) {
+
         Optional<Order> optionalOfOrder = orderRepository.findById(id);
-        Order order = optionalToEntity(optionalOfOrder);
+
+        Order order = optionalOfOrder.orElse(null);
+        if(order == null) log.error("Unable to get an order for there is no such");
+
         return orderMapper.orderToOrderDto(order);
     }
 
     @Override
     public List<OrderDto> getAll() {
+
         List<Order> orderList = orderRepository.findAll();
+
         return orderMapper.orderListToOrderDtoList(orderList);
     }
 
     @Override
     public void add(OrderDto orderDto) {
+
         Order order = orderMapper.orderDtoToOrder(orderDto);
+
         orderRepository.save(order);
+        log.debug("Order has been added: " + order);
     }
 
     @Override
     public void update(OrderDto orderDto) {
+
         Order order = orderMapper.orderDtoToOrder(orderDto);
+
         orderRepository.save(order);
+        log.debug("Order has been updated: " + order);
     }
 
     @Override
     public void deleteById(Integer id) {
+
         Optional<Order> optionalOfOrder = orderRepository.findById(id);
-        Order order = optionalToEntity(optionalOfOrder);
+
+        Order order = optionalOfOrder.orElse(null);
+        if(order == null) log.error("Unable to get an order for there is no such");
+
         OrderDto orderDto = orderMapper.orderToOrderDto(order);
+
         List<ProductDto> productDtoList = orderDto.getProducts();
         for (ProductDto productDto : productDtoList) {
             productService.deleteById(productDto.getId());
         }
+
+        log.trace("Going to delete an order with id " + id);
         orderRepository.deleteById(id);
     }
 
     @Override
     public void refreshCostById(Integer id) {
+
         OrderDto orderDto = getById(id);
+
         Double cost = 0.0;
+
         List<ProductDto> productDtoList = getById(id).getProducts();
         for (ProductDto productDto : productDtoList) {
             cost += productDto.getCost();
         }
+
         orderDto.setCost(cost);
+
         update(orderDto);
     }
 }
--- glovoHillel/src/main/java/org/raghoul818/glovohillel/service/product/ProductServiceImpl.java
+++ glovoHillel/src/main/java/org/raghoul818/glovohillel/service/product/ProductServiceImpl.java
@@ -1,6 +1,7 @@
 package org.raghoul818.glovohillel.service.product;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.raghoul818.glovohillel.dto.product.ProductDto;
 import org.raghoul818.glovohillel.mapper.product.ProductMapper;
 import org.raghoul818.glovohillel.model.product.Product;
@@ -10,47 +11,54 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository productRepository;
     private final ProductMapper productMapper;
 
-    private Product optionalToEntity(Optional<Product> optionalOfProduct) {
-        Product product = new Product();
-        if(optionalOfProduct.isPresent()) {
-            product = optionalOfProduct.get();
-        }
-        return product;
-    }
-
     @Override
     public ProductDto getById(Integer id) {
+
         Optional<Product> optionalOfProduct = productRepository.findById(id);
-        Product product = optionalToEntity(optionalOfProduct);
+
+        Product product = optionalOfProduct.orElse(null);
+        if(product == null) log.error("Unable to get a product for there is no such");
+
         return productMapper.productToProductDto(product);
     }
 
     @Override
     public List<ProductDto> getAll() {
+
         List<Product> productList = productRepository.findAll();
+
         return productMapper.productListToProductDtoList(productList);
     }
 
     @Override
     public void add(ProductDto productDto) {
+
         Product product = productMapper.productDtoToProduct(productDto);
+
         productRepository.save(product);
+        log.debug("Product has been added: " + product);
     }
 
     @Override
     public void update(ProductDto productDto) {
+
         Product product = productMapper.productDtoToProduct(productDto);
+
         productRepository.save(product);
+        log.debug("Product has been updated: " + product);
     }
 
     @Override
     public void deleteById(Integer id) {
+
+        log.trace("Going to delete a product with id " + id);
         productRepository.deleteById(id);
     }
 }
